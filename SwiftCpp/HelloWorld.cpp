091f3d377ab396baab157511763e3a3e47aa4b91
최민한 <chmh0411@gmail.com>
2022-10-22 18:00:54 +0900

    add: cpp file

---
+++ SwiftCpp/HelloWorld.cpp
@@ -0,0 +1,12 @@
+//
+//  HelloWorld.cpp
+//  SwiftCpp
+//
+//  Created by 최민한 on 2022/10/22.
+//
+
+#include "HelloWorld.hpp"
+
+std::string HelloWorld::sayHello() {
+  return "Hello from CPP World!";
+}
---
+++ SwiftCpp/HelloWorld.hpp
@@ -0,0 +1,19 @@
+//
+//  HelloWorld.hpp
+//  SwiftCpp
+//
+//  Created by 최민한 on 2022/10/22.
+//
+
+#ifndef HelloWorld_hpp
+#define HelloWorld_hpp
+
+#include <stdio.h>
+#include <string>
+
+class HelloWorld {
+public:
+  std::string sayHello();
+}
+
+#endif /* HelloWorld_hpp */
