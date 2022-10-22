1a0d356fcd2bf27cacd74f8eb0404f0488d069ed
최민한 <chmh0411@gmail.com>
2022-10-22 18:04:49 +0900

    add: HelloWorldWrapper.mm

---
+++ SwiftCpp/HelloWorldWrapper.mm
@@ -0,0 +1,13 @@
+//
+//  HelloWorldWrapper.m
+//  SwiftCpp
+//
+//  Created by 최민한 on 2022/10/22.
+//
+
+/*
+ 확장자가 m이 아니라 mm인것은 Xcode와 컴파일러에게 이 Obc파일은 cpp을 사용한다고 알리는 용도
+ */
+
+
+#import <Foundation/Foundation.h>
