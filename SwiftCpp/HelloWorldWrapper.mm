f86554aa98a0b157333f02b147976c10525cd4ec
최민한 <chmh0411@gmail.com>
2022-10-22 18:06:43 +0900

    imple: HelloWorldWrapper.mm

--- SwiftCpp/HelloWorldWrapper.mm
+++ SwiftCpp/HelloWorldWrapper.mm
@@ -11,3 +11,17 @@
 
 
 #import <Foundation/Foundation.h>
+#import "HelloWorldWrapper.h"
+#import "HelloWorld.hpp"
+
+@implementation HelloWorldWrapper
+
+- (NSString *) sayHello {
+  HelloWorld helloWorld;
+  std::string helloWorldMessage = helloWorld.sayHello();
+  return [NSString
+          stringWithCString:helloWorldMessage.c_str()
+          encoding:NSUTF8StringEncoding];
+}
+
+@end
