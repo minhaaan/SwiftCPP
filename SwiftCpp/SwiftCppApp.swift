8d272c476fb5e5ded3a960bd72e7ca657f43645e
최민한 <chmh0411@gmail.com>
2022-10-22 17:56:09 +0900

    make project

---
+++ SwiftCpp/SwiftCppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCppApp.swift
+//  SwiftCpp
+//
+//  Created by 최민한 on 2022/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
